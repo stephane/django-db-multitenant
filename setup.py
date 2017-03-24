4352589b55b67c1925b13e5b3f5c523ca4ebd65c
Stéphane Raimbault <stephane.raimbault@gmail.com>
2017-03-24 15:22:43 +0100

    Change package name and bump version to 0.9 to publish

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from os.path import exists
 from setuptools import setup, find_packages
 
 README = 'README.md'
 
+# Change package name to publish my fork on Pypi while waiting for a new upstream release
 setup(
-    name='django-db-multitenant',
-    version='0.1.3',
-    author='mike wakerly',
-    author_email='opensource@hoho.com',
-    packages = find_packages(),
+    name='webstack-django-db-multitenant',
+    version='0.9',
+    author=u'Stéphane Raimbault',
+    author_email='stephane.raimbault@webstack.fr',
+    packages=find_packages(),
     url='https://github.com/mik3y/django-db-multitenant',
     license='BSD',
     description='Multitenant support for Django, using one tenant per database.',
     long_description=open(README).read() if exists(README) else "",
     install_requires=[
-        'Django >= 1.2.0',
+        'Django >= 1.8.0',
     ],
 )
